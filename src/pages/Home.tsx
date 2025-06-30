060c65b7fffb987b4d0fa5b26113e13832dbc5c2
Hassnain Kazmi <hassnainkazmi07@gmail.com>
2025-06-30 13:51:29 +0200

    fix(layout): ensure scroll position resets to top on route navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,7 +34,6 @@ const Home = ({ search }: HomeProps) => {
         bgcolor: "#181818",
         px: { xs: 0.5, sm: 2, md: 3 },
         py: 4,
-        pt: { xs: 7, sm: 9 },
       }}
     >
       <Box
--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -27,7 +27,6 @@ const Shows = () => {
         bgcolor: "#181818",
         px: { xs: 0.5, sm: 2, md: 3 },
         py: 4,
-        pt: { xs: 7, sm: 9 },
       }}
     >
       <Box
